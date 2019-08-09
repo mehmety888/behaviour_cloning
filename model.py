5bcdf21ae44e7db42caac2e80ae959ca346f43ff
mehmety888 <mehmety888@gmail.com>
2019-08-09 23:27:57 +0300

    fix: adjust steering measurements for left and right camera

--- model.py
+++ model.py
@@ -15,19 +15,39 @@ with open('data/driving_log.csv') as csvfile:
     for line in reader:
         lines.append(line)
         
+def load_image(row):
+    filename = row.split('\\')[-1]
+    current_path = "data/IMG/"+filename
+    #load image
+    image = cv2.imread(current_path)
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    return image
+    
 images = []
 measurements = []
-lines = lines [1:]
-for line in lines:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('\\')[-1]
-        current_path = "data/IMG/"+filename
-        image = cv2.imread(current_path)
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
+for line in lines: 
+    # line[0]: center image path
+    # line[1]: left image path
+    # line[2]: right image path
+    # line[3]: steering angle
+    center_image = load_image(line[0])
+    left_image   = load_image(line[1])
+    right_image  = load_image(line[2]) 
+    
+    images.append(center_image)
+    images.append(left_image)
+    images.append(right_image)
+    
+    # correction factor
+    # http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+    correction = 0.2 
+    center_measurement = float(line[3])
+    left_measurement   = float(line[3]) + correction
+    right_measurement  = float(line[3]) - correction
+    
+    measurements.append(center_measurement)
+    measurements.append(left_measurement)
+    measurements.append(right_measurement)
     
 # flipping images and steering measurements
 augmented_images,augmented_measurements = [],[]
